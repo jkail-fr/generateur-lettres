df7f423d42112934e2affa7f0a56bf8ca584b66a
Jkail <boutaultbenoit@yahoo.fr>
2019-07-29 19:48:48 +0200

    Reset reinitialise aussi le chiffre des lettres ... petit oubli

--- script/script.js
+++ script/script.js
@@ -1,33 +1,38 @@
 //fonction pour push dans la div
 //preparation
-var voyelles = ["A","E","I","O","U"];
-var consonnes = ["B","C","D","F","G","H","J","K","L","M","N","P","Q","R","S","T","V","W","X","Z"];
+var voyelles = ["A", "E", "I", "O", "U"];
+var consonnes = ["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Z"];
 nbrLettre = 0;
 
 // on gere le nombre de lettre et son affichage
-function affichageLettres(nombre) {
-    document.querySelector('#total span').innerHTML = nombre;
-}
+function affichageLettres(nombre)
+	{
+	document.querySelector('#total span').innerHTML = nombre;
+	}
+
 affichageLettres(nbrLettre);
 
 //fonction générant les lettres
-function generateLetter(serie) 
+function generateLetter(serie)
 	{
-		var randomNumber = Math.floor(Math.random() * Math.floor(serie.length));
-		return serie[randomNumber];
-		
+	var randomNumber = Math.floor(Math.random() * Math.floor(serie.length));
+	return serie[randomNumber];
+
 	}
 
 //fonction pour l'affichage des lettres	
-function affichagechaine(lettre) {
-	var lettreHTML = "<span>"+lettre+"</span>";
-    document.querySelector('#caracteres').innerHTML += lettreHTML;
-}
+function affichagechaine(lettre)
+	{
+	var lettreHTML = "<span>" + lettre + "</span>";
+	document.querySelector('#caracteres').innerHTML += lettreHTML;
+	}
 
-function resetChaine() {
-    var lettreHTML =null;
-    document.querySelector('#caracteres').innerHTML = lettreHTML;
-}
+function resetChaine()
+	{
+	var lettreHTML = null;
+	document.querySelector('#caracteres').innerHTML = lettreHTML;
+	affichageLettres('0');
+	}
 
 // gestion du clic sur les boutons voyelles / consonnes / reset
 var btnConsonne = document.getElementById('consonne');
@@ -35,67 +40,87 @@ var btnVoyelle = document.getElementById('voyelle');
 var btnReset = document.getElementById('reset');
 
 
-function generateVoyelle() 	
+function generateVoyelle()
 	{
-		var resultat = generateLetter(voyelles);
-		affichagechaine(resultat);
-		nbrLettre = nbrLettre+1;
-		affichageLettres(nbrLettre);
-    };
-	
+	var resultat = generateLetter(voyelles);
+	affichagechaine(resultat);
+	nbrLettre = nbrLettre + 1;
+	affichageLettres(nbrLettre);
+	};
 
-function generateConsonne() 	
+
+function generateConsonne()
 	{
-		var resultat = generateLetter(consonnes);
-		nbrLettre = nbrLettre+1;
-		affichagechaine(resultat);
-		affichageLettres(nbrLettre);
-    };
+	var resultat = generateLetter(consonnes);
+	nbrLettre = nbrLettre + 1;
+	affichagechaine(resultat);
+	affichageLettres(nbrLettre);
+	};
 
-btnConsonne.addEventListener('click', function(){generateConsonne();});
-btnVoyelle.addEventListener('click', function(){generateVoyelle();});
-btnReset.addEventListener('click', function(){resetChaine();});
+btnConsonne.addEventListener('click', function ()
+{
+generateConsonne();
+});
+btnVoyelle.addEventListener('click', function ()
+{
+generateVoyelle();
+});
+btnReset.addEventListener('click', function ()
+{
+resetChaine();
+});
 
 //------------------Gestion des presets
 /*switch case basé sur l'id du bouton pour nombre de boucles ?*/
 
 //on recupere les boutons
-var preset9 =  document.getElementById('9');
-var preset10 =  document.getElementById('10');
-var preset11 =  document.getElementById('11');
-var preset12 =  document.getElementById('12');
+var preset9 = document.getElementById('9');
+var preset10 = document.getElementById('10');
+var preset11 = document.getElementById('11');
+var preset12 = document.getElementById('12');
 
 //fonction qui execute la génération
 
 function generatePreset(presetNumber)
-{ 	
-	generateVoyelle()
-	generateConsonne()
-	generateVoyelle()
-	generateConsonne()
-	for (presetNumber= presetNumber-4; presetNumber > 0; presetNumber--)
 	{
+	generateVoyelle()
+	generateConsonne()
+	generateVoyelle()
+	generateConsonne()
+	for (presetNumber = presetNumber - 4; presetNumber > 0; presetNumber--)
+		{
 		var goRandom = Math.floor(Math.random() * Math.floor('2'));
 		console.log(goRandom);
 		if (goRandom == 0)
-		{	
-		generateVoyelle()
-		}
+			{
+			generateVoyelle()
+			}
 		else
-		{
-		generateConsonne()
+			{
+			generateConsonne()
+			}
 		}
-	}
-};
+	};
 
-preset9.addEventListener('click', function(){generatePreset(9);});
-preset10.addEventListener('click', function(){generatePreset(10);});
-preset11.addEventListener('click', function(){generatePreset(11);});
-preset12.addEventListener('click', function(){generatePreset(12);});
+preset9.addEventListener('click', function ()
+{
+generatePreset(9);
+});
+preset10.addEventListener('click', function ()
+{
+generatePreset(10);
+});
+preset11.addEventListener('click', function ()
+{
+generatePreset(11);
+});
+preset12.addEventListener('click', function ()
+{
+generatePreset(12);
+});
 
 
-
-/*todo :  
+/*todo :
 afficher lettres : OK
 incrementer nombre lettre afficher : OK
 faire les presets : ok
